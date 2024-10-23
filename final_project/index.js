436c8fa39fa348cddfeff2194092edcb78e329d8
xajezade <ali.xajezade@gmail.com>
2024-10-22 20:05:40 -0400

    Added user authentication

--- final_project/index.js
+++ final_project/index.js
@@ -11,7 +11,22 @@ app.use(express.json());
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
 app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+    // check if user is logged in and has a valid token for the session
+    if (req.session.authorization){
+        let token = req.session.authorization['accessToken'];
+
+        //verify JWT token
+        jwt.verify(token, "access", (err, user)=>{
+            if(!err){
+                req.user = user;
+                next();
+            }else{
+                return res.status(403).json({message: "User not authenticated"});
+            }
+        });
+    }else{
+        return res.status(403).json({message: "User not logged in"});
+    }
 });
  
 const PORT =5000;
